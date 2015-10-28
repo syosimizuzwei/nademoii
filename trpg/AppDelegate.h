d49e1e304d01b0d81b5ce24d8e26c2f5db452adf
Shotaro <futatumeno@gmail.com>
2015-10-28 23:32:45 +0900

    Initial Commit

---
+++ trpg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  trpg
+//
+//  Created by Syotaro Shimizu on 2015/10/28.
+//  Copyright (c) 2015年 Syotaro Shimizu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ trpg/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  trpg
+//
+//  Created by Syotaro Shimizu on 2015/10/28.
+//  Copyright (c) 2015年 Syotaro Shimizu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
